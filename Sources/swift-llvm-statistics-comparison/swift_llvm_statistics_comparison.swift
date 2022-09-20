65278c607afa30cdc85ccf2b98d0db1b426d842c
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-09-20 11:00:19 +0200

    finished first version. not tested yet

--- Sources/swift-llvm-statistics-comparison/swift_llvm_statistics_comparison.swift
+++ Sources/swift-llvm-statistics-comparison/swift_llvm_statistics_comparison.swift
@@ -6,39 +6,40 @@ public struct swift_llvm_statistics_comparison {
     public static func main() {
         let statistics = gatherStatisticsFiles(path: "/Users/struewer/git/swift-analysis-benchmark/src/linearConstantPropagation/swift")
         print(statistics)
+        
         // moduleName -> {diff: int, cpp: PhasarStatistics, swift: PhasarStatistics}
         var diffs: [String: Diff] = [:]
         for stat in statistics {
-            // expected module name pattern <testName>_cpp.ll for C++ <testName>_swift.ll
-            // TODO: need to normalize module name here
-            let normalizedModuleName = stat.moduleName
 
-            if diffs[normalizedModuleName] == nil {
-                diffs[normalizedModuleName] = Diff()
-            }
-            
-            // TODO: check how and if changes to diff are propagated inside the dictionary
-            var diff = diffs[normalizedModuleName]!
-            // TODO: get correct file type
-            let fileType: BaseFileType = BaseFileType.swift
-            switch fileType {
-            case .swift:
-                if diff.swift == nil {
-                    diff.swift = stat
-                } else {
-                    print("Duplicate entry found for \(stat.moduleName)")
+            if let normalizedModuleName = try? getNormalized(moduleName: stat.moduleName) {
+                
+                if diffs[normalizedModuleName] == nil {
+                    diffs[normalizedModuleName] = Diff()
                 }
-            case .cpp:
-                if diff.cpp == nil {
-                    diff.cpp = stat
-                } else {
-                    print("Duplicate entry found for \(stat.moduleName)")
+                
+                // TODO: check how and if changes to diff are propagated inside the dictionary
+                var diff = diffs[normalizedModuleName]!
+
+                if let fileType: BaseLanguage = try? getBaseLanguage(moduleName: stat.moduleName) {
+                    switch fileType {
+                    case .swift:
+                        if diff.swift == nil {
+                            diff.swift = stat
+                        } else {
+                            print("Duplicate entry found for \(stat.moduleName)")
+                        }
+                    case .cpp:
+                        if diff.cpp == nil {
+                            diff.cpp = stat
+                        } else {
+                            print("Duplicate entry found for \(stat.moduleName)")
+                        }
+                    }
                 }
-            }
-        
-            if diff.swift != nil && diff.cpp != nil {
-                // TODO: calculate diff here
-                diff.diff = 42
+                
+                if diff.swift != nil && diff.cpp != nil {
+                    diff.diff = calculateDiff(first: diff.swift!, second: diff.cpp!)
+                }
             }
         }
         let sortedDiffs = diffs.sorted(by: { $0.value.diff! < $1.value.diff! })
@@ -48,10 +49,57 @@ public struct swift_llvm_statistics_comparison {
     }
 }
 
-enum BaseFileType {
-    case swift
-    case cpp
+func calculateDiff(first: PhasarStatistics, second: PhasarStatistics) -> Int {
+    // TODO: figure out if this makes sense
+    // it could make sense to weigh the different categories
+    // also this method produces a positive diff for every single value
+    // maybe we should just make the sum positive and let some negative and
+    // some positive values weigh each other? 
+    let insDiff = singleDiff(a: first.instructions, b: second.instructions)
+    let allocaDiff = singleDiff(a: first.allocaInstructions, b: second.allocaInstructions)
+    let callSiteDiff = singleDiff(a: first.callSites, b: second.callSites)
+    let functionsDiff = singleDiff(a: first.functions, b: second.functions)
+    let globalDiff = singleDiff(a: first.globalVariables, b: second.globalVariables)
+    return insDiff + allocaDiff + callSiteDiff + functionsDiff + globalDiff
 }
+
+func singleDiff(a: Int, b: Int)-> Int {
+    var diff = a - b
+    if (diff < 0) {
+        diff = diff * -1
+    }
+    return diff
+}
+
+///  expected input pattern testName_cpp.ll for C++ and testName_swift.ll for Swift
+///  expected output testName
+/// - Parameters:
+///   - moduleName: The value to be normalized.
+func getNormalized(moduleName: String) throws -> String {
+    var res: Substring = Substring(moduleName)
+    if let lastSlashIdx = moduleName.lastIndex(of: "/") {
+        res = moduleName.suffix(from: lastSlashIdx)
+        res.removeFirst()
+        print(res)
+    }
+    if let firstUnderscoreIdx = moduleName.firstIndex(of: "_") {
+        res = res.prefix(upTo: firstUnderscoreIdx)
+        print(res)
+        return String(res)
+    }
+    throw ModuleNameFormatError.normalizationFailed(moduleName: moduleName)
+}
+
+func getBaseLanguage(moduleName: String) throws -> BaseLanguage {
+    if moduleName.contains("_swift.ll") {
+        return BaseLanguage.swift
+    } else if moduleName.contains("_cpp.ll") {
+        return BaseLanguage.cpp
+    }
+
+    throw ModuleNameFormatError.noBaseLanguage(moduleName: moduleName)
+}
+
 func gatherStatisticsFiles(path: String) -> [PhasarStatistics] {
     var statistics: [PhasarStatistics] = []
     let enumerator = FileManager.default.enumerator(atPath: path)
@@ -75,6 +123,7 @@ func readContent(path: String)-> PhasarStatistics? {
     if let content = FileManager.default.contents(atPath: path) {
         if let statistics: PhasarStatistics = try? JSONDecoder().decode(PhasarStatistics.self, from: content) { return statistics}
     }
+    print("Expected statistics at path \(path)")
     return nil
 }
 
@@ -96,4 +145,14 @@ struct PhasarStatistics: Codable {
         case instructions = "Instructions"
         case moduleName = "ModuleName"
     }
+}
+
+enum BaseLanguage {
+    case swift
+    case cpp
+}
+
+enum ModuleNameFormatError: Error {
+    case noBaseLanguage(moduleName: String)
+    case normalizationFailed(moduleName: String)
 }
