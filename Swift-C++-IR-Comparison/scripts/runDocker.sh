9dfbaf6962fa0e5c5ff2b2768b35609b2dd5e1a3
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-10-19 12:57:53 +0200

    docker runs are allowed to fail

--- Swift-C++-IR-Comparison/scripts/runDocker.sh
+++ Swift-C++-IR-Comparison/scripts/runDocker.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 function runDocker {
     workingdirectory=${1%/*}
     fileName=${1##*/}
-    docker run --mount "type=bind,source=${PWD}/$workingdirectory,target=/usr/data/" ghcr.io/secure-software-engineering/phasar:pr-524 -m "/usr/data/$fileName" -S -L --emit-statistic-as-json --project-id "$fileName-results" --out /usr/data
+    docker run --mount "type=bind,source=${PWD}/$workingdirectory,target=/usr/data/" ghcr.io/secure-software-engineering/phasar:pr-524 -m "/usr/data/$fileName" -S -L --emit-statistic-as-json --project-id "$fileName-results" --out /usr/data || echo "$fileName failed to successfully analze."
 }
 cd ..
 
