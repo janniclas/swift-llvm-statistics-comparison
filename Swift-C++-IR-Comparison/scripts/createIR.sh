510286b49d68d5d925f68dcb8e82c6b02e1110ea
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-10-19 11:32:14 +0200

    compile commands are allowed to fail

--- Swift-C++-IR-Comparison/scripts/createIR.sh
+++ Swift-C++-IR-Comparison/scripts/createIR.sh
@@ -8,8 +8,8 @@ function generate_IR {
         newSwiftIRName="$1".noOpt.ll
         fileName=${newSwiftIRName##*/}
 
-        swiftc -emit-ir -parse-as-library -Onone -Xfrontend -disable-llvm-optzns -Xfrontend -disable-swift-specific-llvm-optzns -module-name myModule "$1" | swift demangle > $newSwiftIRNameDemangled
-        swiftc -emit-ir -parse-as-library -Onone -Xfrontend -disable-llvm-optzns -Xfrontend -disable-swift-specific-llvm-optzns -module-name myModule "$1" -o $newSwiftIRName
+        swiftc -emit-ir -parse-as-library -Onone -Xfrontend -disable-llvm-optzns -Xfrontend -disable-swift-specific-llvm-optzns -module-name myModule "$1" | swift demangle > $newSwiftIRNameDemangled || echo "$fileName failed to compile"
+        swiftc -emit-ir -parse-as-library -Onone -Xfrontend -disable-llvm-optzns -Xfrontend -disable-swift-specific-llvm-optzns -module-name myModule "$1" -o $newSwiftIRName || echo "$fileName failed to compile"
     fi
 
     if grep -qE '.cpp$' <<< "$1"; then
@@ -17,7 +17,7 @@ function generate_IR {
         fileName=${1##*/}
         # Somehow the default setting on the M1 MacBook is not to support usual C++ features
         # like a raw string so we had to add the -std=c++20 flag.
-        clang++ -emit-llvm -S -fno-discard-value-names -std=c++20 -working-directory $workingdirectory $fileName -o $fileName.ll
+        clang++ -emit-llvm -S -fno-discard-value-names -std=c++20 -working-directory $workingdirectory $fileName -o $fileName.ll || echo "$fileName failed to compile"
         fileName=${fileName%.cpp}.cpp.ll
     fi
 }
