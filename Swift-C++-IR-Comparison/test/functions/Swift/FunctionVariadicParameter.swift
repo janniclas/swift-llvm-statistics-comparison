fb97b849772391ae9fc044ab9e73314e4b10ba65
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-10-25 10:29:14 +0200

    variadic parameter

---
+++ Swift-C++-IR-Comparison/test/functions/Swift/FunctionVariadicParameter.swift
@@ -0,0 +1,16 @@
+@main
+struct MyMain {
+
+    static func main() {
+        let res = arithmeticMean(3, 8.25, 18.75)
+    }
+
+}
+
+func arithmeticMean(_ numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
