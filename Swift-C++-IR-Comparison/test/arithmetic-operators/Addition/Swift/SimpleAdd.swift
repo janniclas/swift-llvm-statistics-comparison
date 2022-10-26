83bd2b4562ffa459b597fc14a9e92e6706ed5e92
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-10-26 18:56:04 +0200

    autoformat

--- Swift-C++-IR-Comparison/test/arithmetic-operators/Addition/Swift/SimpleAdd.swift
+++ Swift-C++-IR-Comparison/test/arithmetic-operators/Addition/Swift/SimpleAdd.swift
@@ -5,11 +5,12 @@ struct MyMain {
         // The code of this method can't be directly placed inside
         // the main function or it would be removed by the compiler
         // and replaced by 84.
-        let a = simpleAdd(x: 42, y: 42)
+        simpleAdd(x: 42, y: 42)
     }
 
     // We need to store, return, or use the result of the addition to keep
     // the compiler to remove it during optimization.
+    @discardableResult
     static func simpleAdd(x: Int, y: Int) -> Int {
         let tmp = x + y
         return tmp
