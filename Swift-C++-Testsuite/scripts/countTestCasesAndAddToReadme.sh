d1229e30cbcb5e29219b73b290ca58b6be9660f7
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-10-28 14:09:59 +0200

    pre commit hook to auto update readme

--- Swift-C++-Testsuite/scripts/countTestCasesAndAddToReadme.sh
+++
@@ -1,9 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR/../..
-
-counter=0
-for FILE in $(find . -name '*.swift');
-do let counter=counter+1
-done
-echo $counter
-
---
+++ scripts/countTestCasesAndAddToReadme.sh
@@ -0,0 +1,17 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR/..
+
+counterSwift=0
+for FILE in $(find . -name '*.swift');
+do let counterSwift=counterSwift+1
+done
+echo Number Swift Tests $counterSwift
+
+counterCpp=0
+for FILE in $(find . -name '*.swift');
+do let counterCpp=counterCpp+1
+done
+echo Number Swift Tests $counterCpp
+
+echo Generating Readme
+noSwiftCases=$counterSwift noCppCases=$counterCpp envsubst < scripts/README_TEMPLATE.md > README.md
