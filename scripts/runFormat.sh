0da8033e3a88d5eb710260eb2d95099d5bf7cd5d
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-10-28 15:07:31 +0200

    corrected position of swift style

--- scripts/runFormat.sh
+++ scripts/runFormat.sh
@@ -3,8 +3,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 
 if which swift-format >/dev/null; then
-    swift format -i -r ComparisonTool/Sources --configuration swiftStyle.json
-    swift format -i -r Swift-C++-Testsuite --configuration swiftStyle.json
+    swift format -i -r ComparisonTool/Sources --configuration scripts/swiftStyle.json
+    swift format -i -r Swift-C++-Testsuite --configuration scripts/swiftStyle.json
 else
     echo "warning: swift-format not installed"
 fi
