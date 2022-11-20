6a53d516836e7937a6878da87a249cf3055c7b76
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-11-20 14:46:53 +0100

    started multiplatform development for the comparison tooling

--- ComparisonTool/Sources/swift-llvm-statistics-comparison/swift_llvm_statistics_comparison.swift
+++ ComparisonTool/Sources/swift-llvm-statistics-comparison/swift_llvm_statistics_comparison.swift
@@ -1,20 +1,32 @@
 import ArgumentParser
 import Foundation
 
-@main
-@available(macOS 10.15, *)
-struct swift_llvm_statistics_comparison: AsyncParsableCommand {
-    @Option(help: "Directory path to scan for files.")
-    var path: String
-}
-
-@available(macOS 10.15, *)
-extension swift_llvm_statistics_comparison {
-    mutating func run() async throws {
-
-        print("path found \(path)")
-        let diffCalc = DiffCalculator(basePath: path)
-        try diffCalc.run()
-
+#if os(macOS)
+    @main
+    @available(macOS 10.15, *)
+    struct swift_llvm_statistics_comparison: AsyncParsableCommand {
+        @Option(help: "Directory path to scan for files.")
+        var path: String
     }
+
+    @available(macOS 10.15, *)
+    extension swift_llvm_statistics_comparison {
+        mutating func run() async throws {
+            try start(path: path)
+        }
+    }
+#endif
+#if os(Linux)
+    @main
+    struct swift_llvm_statistics_comparison {
+        func main() {
+            print("We don't support linux right now but this might change soon.")
+        }
+    }
+#endif
+
+func start(path: String) throws {
+    print("path found \(path)")
+    let diffCalc = DiffCalculator(basePath: path)
+    try diffCalc.run()
 }
