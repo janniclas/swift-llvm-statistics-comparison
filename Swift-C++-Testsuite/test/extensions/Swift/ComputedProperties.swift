a591e395eac7fae4c2c9f2a4e8cfad830b5b7e20
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-11-21 12:51:39 +0100

    first extension test

---
+++ Swift-C++-Testsuite/test/extensions/Swift/ComputedProperties.swift
@@ -0,0 +1,19 @@
+@main
+struct MyMain {
+
+    static func main() {
+        let oneInch = 25.4.mm
+        print("One inch is \(oneInch) meters")
+        // Prints "One inch is 0.0254 meters"
+        let threeFeet = 3.ft
+        print("Three feet is \(threeFeet) meters")
+    }
+}
+
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
