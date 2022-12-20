ccd67f9a9edcc70de5a606285d7556c5f57070bb
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-12-20 16:22:02 +0100

    code restructuring and inclusion of logging library

--- ComparisonTool/Sources/swift-llvm-statistics-comparison/IRGenerator.swift
+++ ComparisonTool/Sources/swift-llvm-statistics-comparison/IRGenerator.swift
@@ -6,142 +6,8 @@
 //
 
 import Foundation
+import Logging
 import System
-
-struct Statistics {}
-
-class Program {
-    enum PL {
-        case swift
-        case cpp
-    }
-
-    let language: PL
-
-    let name: String
-    let path: String
-
-    init(language: PL, name: String, path: String) {
-        self.language = language
-        self.name = name
-        self.path = path
-        self.irPath = "\(path).ir.ll"
-    }
-
-    var irPath: String
-    var statistics: Statistics?
-}
-
-@available(macOS 12.0, *)
-actor FileWorklist {
-
-    /// Program name -> Programs
-    let programs: [String: (_: Program, _: Program?)]
-    fileprivate var idx: Dictionary<String, (_: Program, _: Program?)>.Index
-
-    var isEmpty: Bool
-
-    init() {
-        self.init(programs: [:])
-    }
-
-    init(programs: [String: (_: Program, _: Program?)]) {
-        self.programs = programs
-        self.idx = self.programs.startIndex
-        self.isEmpty = programs.isEmpty
-    }
-
-    func next() -> (_: Program, _: Program?)? {
-        if isEmpty {
-            return nil
-        }
-        let res = programs[idx]
-        idx = programs.index(after: idx)
-        if idx == programs.endIndex {
-            isEmpty = true
-        }
-        return res.value
-    }
-
-}
-@available(macOS 13.0, *)
-struct Worker {
-
-    init(workerNumber: UInt8) {
-        self.workerNumber = workerNumber
-        print("Initialized worker \(workerNumber)")
-    }
-
-    static let maximumNumberOfTasks: UInt8 = 8  //TODO: query number of physical cores - 1 and use this here
-    let workerNumber: UInt8
-    let compiler = Compiler()
-
-    struct Compiler {
-        let cppCompilerPath = FilePath("/usr/bin/clang++")
-        let cppArgs = ["-emit-llvm", "-g", "-S", "-fno-discard-value-names", "-std=c++20", "-o"]
-
-        let swiftcPath = FilePath("/usr/bin/swiftc")
-        let swiftArgs = [
-            "-emit-ir", "-g", "-parse-as-library", "-Onone", "-Xfrontend", "-disable-llvm-optzns", "-Xfrontend",
-            "-disable-swift-specific-llvm-optzns", "-module-name", "myModule", "-o",
-        ]
-
-        private func getCppArgs(_ program: Program) -> [String] {
-            var res = cppArgs
-            res.insert(program.path, at: 5)
-            res.append(program.irPath)
-            return res
-        }
-
-        private func getSwiftArgs(_ program: Program) -> [String] {
-            var res = swiftArgs
-            res.insert(program.path, at: 10)
-            res.append(program.irPath)
-
-            return res
-        }
-
-        private func getCompileConfig(_ program: Program) -> (filePath: FilePath, args: [String]) {
-
-            if program.language == .cpp {
-                return (cppCompilerPath, getCppArgs(program))
-            } else {
-                return (swiftcPath, getSwiftArgs(program))
-            }
-        }
-
-        func compileProgram(_ program: Program) async {
-            let config = getCompileConfig(program)
-            let p = Process()
-
-            p.executableURL = URL(filePath: config.filePath)
-            p.arguments = config.args
-
-            p.terminationHandler = { (process) in
-                print("\ndidFinish: \(!process.isRunning)")
-                print("end")
-            }
-
-            do {
-                try p.run()
-            } catch {
-                print("error occured")
-            }
-            p.waitUntilExit()
-        }
-    }
-
-    private func processProgram(_ program: Program) async {
-        await compiler.compileProgram(program)
-    }
-
-    func work(_ programs: (_: Program, _: Program?)) async {
-        await processProgram(programs.0)
-        if let p1 = programs.1 {
-            await processProgram(p1)
-        }
-    }
-}
 
 func getPrograms(_ basePath: String) -> [String: (_: Program, _: Program?)] {
 
@@ -169,7 +35,7 @@ func getPrograms(_ basePath: String) -> [String: (_: Program, _: Program?)] {
     return programDictionary
 }
 
-func getProgramFromPath(_ path: String, type: Program.PL) -> Program {
+private func getProgramFromPath(_ path: String, type: Program.PL) -> Program {
     let fh = FileHelperFactory.getFileHelper()
     var fileName: String
     do {
@@ -181,27 +47,104 @@ func getProgramFromPath(_ path: String, type: Program.PL) -> Program {
     return Program(language: type, name: fileName, path: path)
 }
 
-@available(macOS 13.0, *)
-func generateIr(basePath: String) async {
-    // fill worklist
-    let programs = getPrograms(basePath)
-    let workList = FileWorklist(programs: programs)
-    // work worklist
-    await withTaskGroup(of: Void.self) { taskGroup in
-        for i in 0..<Worker.maximumNumberOfTasks {
-            taskGroup.addTask {
-                let worker = Worker(workerNumber: i)
-                while let item = await workList.next() {
-                    print(
-                        "Starting work on: \(item.0.name) and: \(item.1?.name ?? "") with worker no \(i)"
-                    )
-                    await worker.work(item)
-                    print(
-                        "Finished work on swift: \(item.0.name) and cpp: \(item.1?.name ?? "") with worker no \(i)"
-                    )
-                }
+protocol Compiler {
+    func compileProgram(_ program: Program) async throws
+}
+
+struct CompilerFactory {
+
+    static var compiler: Compiler?
+    static func getCompiler() -> Compiler {
+        if compiler != nil {
+            return compiler!
+        }
+        #if os(macOS)
+            if #available(macOS 13.0, *) {
+                compiler = CompilerMacOS()
+                return compiler!
             }
+        #endif
+        compiler = GeneralCompiler()
+        return compiler!
+    }
+}
+
+class GeneralCompiler: Compiler {
+
+    public enum CompilerError: Error {
+        case compilerUrlNotFound(url: String)
+        case compilationFailed(program: Program)
+    }
+
+    private let swiftArgs = [
+        "-emit-ir", "-g", "-parse-as-library", "-Onone", "-Xfrontend", "-disable-llvm-optzns", "-Xfrontend",
+        "-disable-swift-specific-llvm-optzns", "-module-name", "myModule", "-o",
+    ]
+    private let cppArgs = ["-emit-llvm", "-g", "-S", "-fno-discard-value-names", "-std=c++20", "-o"]
+
+    internal let logger = Logger(label: "com.struewer.llvm.statistics.compiler")
+
+    internal let cppCompilerPath = "/usr/bin/clang++"
+    internal let swiftcPath = "/usr/bin/swiftc"
+
+    func compileProgram(_ program: Program) async throws {
+        logger.debug("compileProgram called with \(program)")
+
+        let config = try getCompileConfig(program)
+        let p = Process()
+
+        p.executableURL = config.url
+        p.arguments = config.args
+
+        try p.run()
+
+        p.waitUntilExit()
+    }
+
+    internal func getCompileConfig(_ program: Program) throws -> (url: URL, args: [String]) {
+        if program.language == .cpp {
+            if let url = URL(string: cppCompilerPath) {
+                return (url, getCppArgs(program))
+            }
+            throw CompilerError.compilerUrlNotFound(url: swiftcPath)
+        } else {
+            if let url = URL(string: swiftcPath) {
+                return (url, getSwiftArgs(program))
+            }
+            throw CompilerError.compilerUrlNotFound(url: cppCompilerPath)
         }
     }
-    print("Task group finished")
+
+    internal func getCppArgs(_ program: Program) -> [String] {
+        var res = cppArgs
+        res.insert(program.path, at: 5)
+        res.append(program.irPath)
+        return res
+    }
+
+    internal func getSwiftArgs(_ program: Program) -> [String] {
+        var res = swiftArgs
+        res.insert(program.path, at: 10)
+        res.append(program.irPath)
+
+        return res
+    }
+}
+
+@available(macOS 13.0, *)
+class CompilerMacOS: GeneralCompiler {
+
+    override init() {
+        super.init()
+        logger.debug("MacOS Compiler initialized")
+        // TODO: add config file to inject custom paths and settings
+    }
+
+    override internal func getCompileConfig(_ program: Program) -> (url: URL, args: [String]) {
+        if program.language == .cpp {
+            return (URL(filePath: cppCompilerPath), getCppArgs(program))
+        } else {
+            return (URL(filePath: swiftcPath), getSwiftArgs(program))
+        }
+    }
 }
---
+++ ComparisonTool/Sources/swift-llvm-statistics-comparison/Worker.swift
@@ -0,0 +1,99 @@
+//
+//  File.swift
+//
+//
+//  Created by Jan-Niclas Strüwer on 20.12.22.
+//
+
+import Foundation
+import Logging
+
+struct Statistics {}
+
+class Program {
+    enum PL {
+        case swift
+        case cpp
+    }
+
+    let language: PL
+
+    let name: String
+    let path: String
+
+    init(language: PL, name: String, path: String) {
+        self.language = language
+        self.name = name
+        self.path = path
+        self.irPath = "\(path).ir.ll"
+    }
+
+    var irPath: String
+    var statistics: Statistics?
+}
+
+actor FileWorklist {
+
+    /// Program name -> Programs
+    let programs: [String: (_: Program, _: Program?)]
+    fileprivate var idx: Dictionary<String, (_: Program, _: Program?)>.Index
+
+    var isEmpty: Bool
+
+    init() {
+        self.init(programs: [:])
+    }
+
+    init(programs: [String: (_: Program, _: Program?)]) {
+        self.programs = programs
+        self.idx = self.programs.startIndex
+        self.isEmpty = programs.isEmpty
+    }
+
+    func next() -> (_: Program, _: Program?)? {
+        if isEmpty {
+            return nil
+        }
+        let res = programs[idx]
+        idx = programs.index(after: idx)
+        if idx == programs.endIndex {
+            isEmpty = true
+        }
+        return res.value
+    }
+}
+
+struct Worker {
+
+    init(workerNumber: UInt) {
+        self.workerNumber = workerNumber
+        print("Initialized worker \(workerNumber)")
+
+    }
+    internal let logger = Logger(label: "com.struewer.llvm.statistics.worker")
+    static let maximumNumberOfTasks: UInt = physicalCoresCount()
+
+    private static func physicalCoresCount() -> UInt {
+
+        var size: size_t = MemoryLayout<UInt>.size
+        var coresCount: UInt = 0
+
+        sysctlbyname("hw.perflevel0.physicalcpu", &coresCount, &size, nil, 0)
+
+        return coresCount
+    }
+
+    let workerNumber: UInt
+    let compiler = CompilerFactory.getCompiler()
+
+    private func processProgram(_ program: Program) async throws {
+        try await compiler.compileProgram(program)
+    }
+
+    func work(_ programs: (_: Program, _: Program?)) async throws {
+        try await processProgram(programs.0)
+        if let p1 = programs.1 {
+            try await processProgram(p1)
+        }
+    }
+}
