2b98c7f258f64153f899e7e863ff47748b6ece0b
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2023-02-13 09:16:14 +0100

    added taint test case for string constructors

---
+++ Swift-C++-Testsuite/test/stringsAndCharacters/Swift/SimpleStringDefinitionTaintTest.swift
@@ -0,0 +1,17 @@
+@main
+struct MyMain {
+
+    static func source() -> String {
+        print("Source")
+        return "Secret"
+    }
+
+    static func sink(leaked: String) {
+        print("Leaked \(leaked).")
+    }
+
+    static func main() {
+        var secret = source()
+        sink(leaked: secret)
+    }
+}
