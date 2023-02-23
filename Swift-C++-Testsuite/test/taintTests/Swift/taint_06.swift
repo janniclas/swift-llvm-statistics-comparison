ea5d0da5fa890a2fc2fea596c5925f71336a212e
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2023-02-23 13:48:22 +0100

    updated taint tests

---
+++ Swift-C++-Testsuite/test/taintTests/Swift/taint_06.swift
@@ -0,0 +1,13 @@
+
+@main
+struct MyMain {
+    static func source() -> Int {
+        return 0
+    }
+    static func sink(_ p: Int) {
+      print(p)
+    }
+    static func main() {
+        sink(CommandLine.arguments.count)
+    }
+}
